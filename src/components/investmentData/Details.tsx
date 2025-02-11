45178fcace6ec7d4078623ffad2c64c6729d6bcb
xanoosh <xanoosh@gmail.com>
2025-02-11 19:59:01 +0100

    dialog details spacing

--- src/components/investmentData/Details.tsx
+++ src/components/investmentData/Details.tsx
@@ -30,7 +30,7 @@ export default function Details({
         </div>
         <div className="flex flex-col gap-2 py-2 px-5 bg-white/5 rounded-md">
           <div className="flex justify-between text-white">
-            <p className="font-semibold">Inflation adjusted value:</p>
+            <p className="font-semibold">Inflation adjusted:</p>
             <p>{formatNumber(investmentData.details.inflationAdjusted)}</p>
           </div>
           <p className="text-slate-300 text-xs">
